0c9efdaf044672f90d0a9fa1e040ada5ea70bdf3
utnso <pablooib81@gmail.com>
2019-04-07 16:55:42 -0300

    Agrego archivo

---
+++ LFS/src/conexiones.c
@@ -0,0 +1,68 @@
+#include "servidor.h"
+
+int esperarCliente(int socketServidor)
+{
+	struct sockaddr_in direccionCliente;
+	int tamanioDireccion = sizeof(struct sockaddr_in);
+
+	int socketCliente = accept(socketServidor, (void*) &direccionCliente, &tamanioDireccion);
+
+	log_info(logger, "Se conecto un cliente!");
+
+	return socketCliente;
+}
+
+int recibirOperacion(int socketCliente)
+{
+	int codigoOperaciones;
+	if(recv(socketCliente, &codigoOperacion, sizeof(int), MSG_WAITALL) != 0)
+		return codigoOperacion;
+	else
+	{
+		close(socketCliente);
+		return -1;
+	}
+}
+
+void* recibirBuffer(int* size, int socketCliente)
+{
+	void * buffer;
+
+	recv(socketCliente, size, sizeof(int), MSG_WAITALL);
+	buffer = malloc(*size);
+	recv(socketCliente, buffer, *size, MSG_WAITALL);
+
+	return buffer;
+}
+
+void recibirMensaje(int socketCliente)
+{
+	int size;
+	char* buffer = recibirBuffer(&size, socketCliente);
+	log_info(logger, "Me llego el mensaje %s", buffer);
+	free(buffer);
+}
+
+//podemos usar la lista de valores para poder hablar del for y de como recorrer la lista
+t_list* recibirPaquete(int socket_cliente)
+{
+	int size;
+	int desplazamiento = 0;
+	void * buffer;
+	t_list* valores = list_create();
+	int tamanio;
+
+	buffer = recibirBuffer(&size, socket_cliente);
+	while(desplazamiento < size)
+	{
+		memcpy(&tamanio, buffer + desplazamiento, sizeof(int));
+		desplazamiento+=sizeof(int);
+		char* valor = malloc(tamanio);
+		memcpy(valor, buffer+desplazamiento, tamanio);
+		desplazamiento+=tamanio;
+		list_add(valores, valor);
+	}
+	free(buffer);
+	return valores;
+	return NULL;
+}
