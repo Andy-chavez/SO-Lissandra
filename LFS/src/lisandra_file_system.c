4c0d1d3936a1288834f305eb59082f7a8ec92985
facu humphreys <facu2409@hotmail.com>
2019-04-07 13:16:39 -0300

    switch de api

--- LFS/src/lisandra_file_system.c
+++ LFS/src/lisandra_file_system.c
@@ -11,7 +11,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+int main(int argc, char* argv[]) {
+	int caso;
+	switch (caso){
+		case 1:
+			//Select
+			break;
+		case 2:
+			//
+	}
 	return EXIT_SUCCESS;
 }
