d42e43900529ee4d8a0725ebb292b9b45e0f6dfa
Andy-chavez <andychavez02@gmail.com>
2019-04-07 17:59:56 -0300

    Agrego switch API

--- Kernel/Api.c
+++ Kernel/Api.c
@@ -1,10 +1,30 @@
-#include<iostream>
+/*
+ ============================================================================
+ Name        : lisandra_file_system.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
 #include<stdio.h>
+
+
+typedef enum{
+	IN,
+	CR,
+	DT,
+	DA,
+	DR,
+	RU,
+	JO,
+	AD
+
+}casos;
 
 int main()
 {
 
 	return 0;
 }
-
- }
