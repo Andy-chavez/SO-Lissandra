c8bc406d50e5b3debfdb7a965ce1a5ab92a85877
BarchiFranco <barchiesiefranco@gmail.com>
2019-04-10 00:00:32 -0300

    Subo el Api de memoria

---
+++ memoria/Api.c
@@ -0,0 +1,45 @@
+/*
+ ============================================================================
+ Name        : Api.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include<stdio.h>
+
+
+typedef enum{
+	IN,
+	CR,
+	DT,
+	DA,
+	DR,
+	JO,
+
+}casos;
+
+int main()
+{
+	casos CASO;
+	switch(CASO){
+	case IN:
+		break;
+	case CR:
+		break;
+	case DT:
+		break;
+	case DA:
+		break;
+	case DR:
+		break;
+	case JO:
+		break;
+	default:
+		break;
+
+	}
+	return 0;
+}
