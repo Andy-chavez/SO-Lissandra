e4efeb6e0dda6d8b3a09eff4a2bbbbe776a67c49
facu humphreys <facu2409@hotmail.com>
2019-04-14 22:04:43 -0300

    agregado de creacion de logger, servidor y archivos de compactacion y fileSystem

--- LFS/Debug/src/subdir.mk
+++ LFS/Debug/src/subdir.mk
@@ -4,12 +4,21 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../src/compactador.c \
+../src/conexiones.c \
+../src/fileSystem.c \
 ../src/lisandra_file_system.c 
 
 OBJS += \
+./src/compactador.o \
+./src/conexiones.o \
+./src/fileSystem.o \
 ./src/lisandra_file_system.o 
 
 C_DEPS += \
+./src/compactador.d \
+./src/conexiones.d \
+./src/fileSystem.d \
 ./src/lisandra_file_system.d 
 
 
