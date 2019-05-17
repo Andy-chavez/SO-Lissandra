3b1f133cc3e264741b996c5e8da45020a99a52a8
BarchiFranco <barchiesiefranco@gmail.com>
2019-05-17 01:52:57 -0300

    Agrego un par de operaciones sobre segmentos y paginas

--- memoria/operacionesMemoria.h
+++ memoria/operacionesMemoria.h
@@ -35,7 +35,6 @@ typedef struct {
 	flagModificado flag;
 } pagina;
 
-
 typedef struct {
 	char *nombreTabla;
 	t_list* paginas;
@@ -48,8 +47,6 @@ typedef struct {
 	int *seeds;
 } memoria;
 
-
-
 memoria* inicializarMemoria(int tamanio){
 	memoria* nuevaMemoria;
 
@@ -61,29 +58,16 @@ memoria* inicializarMemoria(int tamanio){
 
 }
 
-/*
-char* select(char*nombreTabla,int key){
-
-	if(existeSegmentoDeTabla(memoriaPrincipal,nombreTabla)){
-		segmento segmento = (segmento) encontrarSegmentoPorNombre(memoriaPrincipal,nombreTabla);
-
-		if(segmentoTieneKey(segmento,key){
-			return valuePagina(segmento,key);
-		}
-		else{
-			return pedirPaginaLFS(segmento,key);
-		}
-	}
-	else{
-	return pedirSegmentoLFS(nombreTabla,key);
-	}
-}*/
-
-
-
-bool* igualNombre(segmento segmento,void* nombreBase){
-	char* nombre = (char*)nombreBase;
-	return string_equals_ignore_case(segmento.nombreTabla,nombre);
+void agregarSegmento(memoria* memoria,registro* primerRegistro,char* tabla ){
+	segmento segmentoNuevo;
+	segmentoNuevo.nombreTabla = tabla;
+	segmentoNuevo.paginas = list_create();
+	pagina primerPagina;
+	primerPagina.unaPagina = primerRegistro;
+	primerPagina.numeroPagina = 0;
+	//list_add(segmentoNuevo.paginas,(void*) primerPagina);	no estoy sabiendo como meter esto
+	// Habría que comprobar que hay espacio
+	//list_add(memoria.tablaSegmentos,(void*) segmentoNuevo);    claramente asi no
 }
 
 //--------------------Definicion de t_list's para dos Argumentos-------------------------------------------------
@@ -135,10 +119,79 @@ void* list_find_2(t_list *self, bool(*condition)(void*,void*),void* argumento) {
 
 //---------------------------------------------------------------------------------------------------------------
 
-bool existeSegmentoDeTabla(memoria* memoria,char* tablaNombre){
-	return list_any_satisfy_2(memoria->tablaSegmentos,(void*)igualNombre,(void*)tablaNombre);
+bool igualNombreSegmento(segmento* segmento,void* nombreBase){
+	char* nombre = (char*)nombreBase;
+	return string_equals_ignore_case(segmento->nombreTabla,nombre);
 }
 
-void* encontrarSegmentoPorNombre(memoria* memoria,char* tablaNombre){
-	return list_find_2(memoria->tablaSegmentos,(void*)igualNombre,(void*) tablaNombre);
+bool existeSegmentoDeTabla(memoria* memoria,char* tablaNombre){
+	return list_any_satisfy_2(memoria->tablaSegmentos,(void*)igualNombreSegmento,(void*)tablaNombre);
 }
+
+segmento* encontrarSegmentoPorNombre(memoria* memoria,char* tablaNombre){
+	return list_find_2(memoria->tablaSegmentos,(void*)igualNombreSegmento,(void*) tablaNombre);  //esta bien esto? el list_add devuelve *void
+}
+
+bool igualKeyPagina(pagina* pagina,void* keyDada){
+	int key = (int) keyDada;
+	return pagina->unaPagina->key == key;
+}
+
+bool segmentoTieneKey(segmento* segmento,int keyDada){
+	return list_any_satisfy_2(segmento->paginas,(void*)igualKeyPagina,(void*)keyDada);
+}
+
+pagina* encontrarPaginaPorKey(segmento* segmento, int key){
+	return list_find_2(segmento->paginas,(void*)igualKeyPagina,(void*) key);
+}
+char* valuePagina(segmento* segmento, int key){
+	pagina* paginaEncontrada = encontrarPaginaPorKey(segmento,key);
+	return (char*)paginaEncontrada->unaPagina->value;
+}
+
+bool hayEspacioMemoria(memoria* memoria, int espacioPedido){ // Supongo que pedimos espacio porque puede quedar 1 de espacio y nunca se llena llena
+	return (memoria->limite-memoria->base) > espacioPedido;
+}
+
+/*
+void select(char*nombreTabla,int key){
+
+	if(existeSegmentoDeTabla(memoriaPrincipal,nombreTabla)){
+		segmento segmento = (segmento) encontrarSegmentoPorNombre(memoriaPrincipal,nombreTabla);
+
+		if(segmentoTieneKey(segmento,key)){
+			printf ("El valor es %s",valuePagina(segmento,key));
+		}
+		else{
+			if(hay espacio) pedirPaginaLFS(segmento,key);
+			else { journal(); pedirPaginaLFS(segmento,key);}
+		}
+	}
+	error?, supongo que lo pide, no?
+}*/
+
+/*
+void insert(char*nombreTabla,int key,char*value, memoria* memoriaPrincipal){ //la memoria no se bien como tratarla, por ahora la paso para que "funque"
+	if(existeSegmentoDeTabla(memoriaPrincipal,nombreTabla)){
+		segmento* segmento = encontrarSegmentoPorNombre(memoriaPrincipal,nombreTabla);
+
+		if(segmentoTieneKey(segmento,key)){
+			pagina* paginaEncontrada = encontrarPaginaPorKey(segmento,key);
+			paginaEncontrada->unaPagina->value = value;
+			paginaEncontrada->unaPagina->timestamp = time(NULL);
+			paginaEncontrada->flag = SI;
+			list_replace_and_destroy_element(segmento->paginas,paginaEncontrada->numeroPagina,paginaEncontrada,free);
+			//Todo esto lo deberiamos mandar en una funcioncita aparte de "cambiarPagina" o algo asi?
+		}
+
+	}
+
+	else{
+		registro* registroAGuardar;
+		registroAGuardar->key = key;
+		registroAGuardar->timestamp=time(NULL);
+		registroAGuardar->value=(void*)value;
+		agregarSegmento(memoriaPrincipal,registroAGuardar,nombreTabla);
+	}
+}
+*/
