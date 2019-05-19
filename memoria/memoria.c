a11a1a5d527299ff6447817c1833c64ffcd5cb44
BarchiFranco <barchiesiefranco@gmail.com>
2019-05-19 17:07:50 -0300

    Realizado Mini Handshake

--- memoria/memoria.c
+++ memoria/memoria.c
@@ -9,10 +9,17 @@
 #include <string.h>
 #include <stdio.h>
 #include <commonsPropias/conexiones.h>
+#include <commonsPropias/serializacion.h>
 #include "operacionesMemoria.h"
 
 #define TAMANIOSEGMENTO 10// esto va a estar en un archivo de config
 
+
+int maximoValue;
+void* bufferConLFS;
+pthread_mutex_t mutexBufferLFS = 0;
+
+//------------------------------------------------------------------------
 
 char* pruebaDeRecepcion(void* buffer) {
 	return (char*) buffer;
@@ -23,7 +30,6 @@ void envioDePrueba(int socketServidor, char* mensajeAEnviar) {
 	int tamanioMensajeAEnviar = strlen(mensajeAEnviar) + 1;
 	enviar(socketServidor, (void*) mensajeAEnviar, tamanioMensajeAEnviar);
 }
-
 
 
 void liberarConfigYLogs(configYLogs *archivos) {
@@ -38,12 +44,12 @@ void *clienteLFS(void* arg) {
 	char* fileSystemIP = config_get_string_value(archivosDeConfigYLog->config, "IPFILESYSTEM");
 	char* fileSystemPuerto = config_get_string_value(archivosDeConfigYLog->config, "PUERTOFILESYSTEM");
 
-	int socketClienteKernel = crearSocketCliente(fileSystemIP,fileSystemPuerto);
+	int socketClienteLFS = crearSocketCliente(fileSystemIP,fileSystemPuerto);
 
 	char* mensaje = (char*) arg;
-	enviar(socketClienteKernel, mensaje, (strlen(mensaje)+1)*sizeof(char));
+	enviar(socketClienteLFS, mensaje, (strlen(mensaje)+1)*sizeof(char));
 
-	cerrarConexion(socketClienteKernel);
+	cerrarConexion(socketClienteLFS);
 }
 
 void *servidorMemoria(void* arg){
@@ -65,8 +71,6 @@ void *servidorMemoria(void* arg){
 			log_info(archivosDeConfigYLog->logger, "ERROR: Socket Defectuoso");
 			continue;
 		}
-
-
 		//void* buffer = envioDePrueba("hola");
 		char* mensaje = (char*) recibir(socketKernel); // interface( deserializarOperacion( buffer , 1 ) )
 
@@ -83,12 +87,45 @@ void *servidorMemoria(void* arg){
 
 }
 
+void handshake(void *arg){
+	configYLogs *archivosDeConfigYLog = (configYLogs*) arg;
+
+	char* mensajeALFS = "Hola Lissandra, soy Memoria, un placer.";
+	clienteLFS((void*) mensajeALFS);
+
+	int socketServidorMemoria = crearSocketServidor(config_get_string_value(archivosDeConfigYLog->config, "PUERTOFILESYSTEM"));
+
+	log_info(archivosDeConfigYLog->logger, "Esperando recibir Value de LFS.");
+
+	int socketLFS = aceptarCliente(socketServidorMemoria);
+
+	bufferConLFS = recibir(socketLFS);
+	// maximoValue = deserializarMaxValue(*bufferConLFS);
+	log_info(archivosDeConfigYLog->logger, "Establecido el tamanio maximo value de Pagina como: %d", maximoValue);
+
+	pthread_mutex_lock(&mutexBufferLFS);
+	free(mensajeALFS);
+}
+
+void pedirALFS(operacionLQL* operacion){
+
+	pthread_mutex_lock(&mutexBufferLFS);
+		bufferConLFS = serializarOperacionLQL(operacion);
+		clienteLFS(bufferConLFS);
+	pthread_mutex_unlock(&mutexBufferLFS);
+
+}
+
 int main() {
 	//pthread_t threadServer; //threadCliente, threadTimedJournal, threadTimedGossiping;
 	configYLogs *archivosDeConfigYLog = malloc(sizeof(configYLogs));
 
 	archivosDeConfigYLog->config = config_create("../memoria.config");
 	archivosDeConfigYLog->logger = log_create("memoria.log", "MEMORIA", 1, LOG_LEVEL_INFO);
+
+	//handshake((void*) archivosDeConfigYLog);
+
+	//memoriaPrincipal = inicializarMemoria(tamanio);
 
 	//pthread_create(&threadServer,NULL,servidorMemoria,(void*) archivosDeConfigYLog);
 	//pthread_create(&threadCliente, NULL, clienteKernel, archivosDeConfigYLog);
