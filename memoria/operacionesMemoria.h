5d5c49056c5524de6fa4a39d7415c0d8424fd681
Maxi Feldman <maxifeldman@hotmail.com>
2019-06-04 23:19:52 -0300

    ups

--- memoria/operacionesMemoria.h
+++ memoria/operacionesMemoria.h
@@ -14,13 +14,6 @@ void inicializarProcesoMemoria() {
 	inicializarSemaforos();
 }
 
-void empezarFinalizacion() {
-	crearSocketCliente(config_get_string_value(ARCHIVOS_DE_CONFIG_Y_LOG->config, "IPMEMORIA"), config_get_string_value(ARCHIVOS_DE_CONFIG_Y_LOG->config, "PUERTO"));
-	sem_wait(&MUTEX_PROCESO_ABIERTO);
-	PROCESO_ABIERTO = 0;
-	sem_post(&MUTEX_PROCESO_ABIERTO);
-}
-
 memoria* inicializarMemoria(datosInicializacion* datosParaInicializar, configYLogs* ARCHIVOS_DE_CONFIG_Y_LOG) {
 	memoria* nuevaMemoria = malloc(sizeof(memoria));
 	int tamanioMemoria = config_get_int_value(ARCHIVOS_DE_CONFIG_Y_LOG->config, "TAMANIOMEM");
@@ -54,8 +47,6 @@ void inicializarSemaforos() {
 	sem_init(&BINARIO_SOCKET_KERNEL, 0, 1);
 	sem_init(&MUTEX_LOG, 0, 1);
 	sem_init(&MUTEX_SOCKET_LFS, 0, 1);
-	sem_init(&MUTEX_PROCESO_ABIERTO, 0, 1);
-	sem_init(&BINARIO_PROCESO_ABIERTO, 0, 1);
 }
 
 // ------------------------------------------------------------------------ //
