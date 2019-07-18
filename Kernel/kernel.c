67430710ca2c6f39c28089e7e354f51cac594e18
Andy-chavez <andychavez02@gmail.com>
2019-07-18 13:33:46 -0300

    pruebo metrics con otra configuracion

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -27,11 +27,11 @@ int main(int argc, char *argv[]){
 	pthread_t threadDescribe;
 	pthread_t threadGossip;
 	pthread_t threadMetrics;
-	/*if(argc==1){ todo descomentar esto para entregar
+	if(argc==1){
 		printf("Pruebe ingresando el path del archivo de configuracion como parametro del kernel.\n");
 		return EXIT_FAILURE;
-	}*/
-	pathConfig ="/home/utnso/workspace/tp-2019-1c-Why-are-you-running-/Kernel/KER_CONFIG";//(char*) argv[1];// "/home/utnso/workspace/tp-2019-1c-Why-are-you-running-/Kernel/KER_CONFIG";
+	}
+	pathConfig =(char*) argv[1];//"/home/utnso/workspace/tp-2019-1c-Why-are-you-running-/Kernel/KER_CONFIG";// "/home/utnso/workspace/tp-2019-1c-Why-are-you-running-/Kernel/KER_CONFIG";
 	kernel_inicializarVariablesYListas();
 	if(kernel_inicializarMemoria()==-1){
 		printf("No memory to initialize\n");
